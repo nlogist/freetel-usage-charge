5175fb3e2b6e261ee830ab3a49229769527a34d6
nlogcode <nlogcode@users.noreply.github.com>
2017-01-28 02:22:14 +0900

    freetel.pl updated.
    Saving mode site https://mypage.freetel.jp/SavingMode/ has been changed on January 24, 2017.

--- freetel.pl
+++ freetel.pl
@@ -28,15 +28,23 @@ my $scraper1 = scraper {
 my $scraper2 = scraper {
     process '//div[contains(@class, "row row-extension va-middle")]/div[contains(@class, "col-xs-6 col-sm-3 align-right")]/span[1]', 'charge[]' => 'TEXT';
 };
+my $scraper3 = scraper {
+    process '//*[@id="contents"]/div[1]/div[2]/div[2]/span[1]', 'usage[]' => 'TEXT';
+};
 $mech->get('https://mypage.freetel.jp/SavingMode') or die;
 my $result1 = $scraper1->scrape($mech->content);
 $mech->get('https://mypage.freetel.jp/Specification/thisMonth/' . $dt->strftime('%Y%m')) or die;
 my $result2 = $scraper2->scrape($mech->content);
 
 for (my $i = 0; $i <= $#{$result1->{phone}}; $i++) {
-    ${$result1->{usage}}[$i] =~ /(\S+)B$/;
-    my $usage = int(unformat_number($1, base => 1024));
     my $charge = defined(${$result2->{charge}}[$i]) ? unformat_number(${$result2->{charge}}[$i]) : 0;
-    print $$, ":", $dt->strftime('%Y%m%d:%H%M%S.%3N'), "\t", ${$result1->{phone}}[$i], "\t", $usage, "\t", $charge, "\n";
-    $mech->get("http://maker.ifttt.com/trigger/" . $event . "/with/key/" . $secret_key . "?value1=" . ${$result1->{phone}}[$i] . "&value2=" . $usage . "&value3=" . $charge) if ($event ne '') or die;
+    my $phone = ${$result1->{phone}}[$i];
+    (my $phone_undashed = $phone) =~ s/-//g;
+    $mech->get('https://mypage.freetel.jp/SavingMode/saveModeDetail/' . $phone_undashed) or die;
+    my $result3 = $scraper3->scrape($mech->content);
+    ${$result3->{usage}}[0] =~ /(\S+)B$/;
+    my $usage = int(unformat_number($1, base => 1024));
+    print $$, ":", $dt->strftime('%Y%m%d:%H%M%S.%3N'), "\t", $phone, "\t", $usage, "\t", $charge, "\n";
+    $mech->get("http://maker.ifttt.com/trigger/" . $event . "/with/key/" . $secret_key . "?value1=" . $phone . "&value2=" . $usage . "&value3=" . $charge)
+      if ($event ne '') or die;
 }
